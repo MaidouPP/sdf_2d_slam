16979fa634d5559b35146992b28a92fc1bec5f2b
MaidouPP <leept416@gmail.com>
2019-05-17 00:05:58 -0700

    Simple occupancy map verification

--- core/grid_map.py
+++ core/grid_map.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python
-
+import matplotlib.pyplot as plt
+plt.ion()
 import numpy as np
 import os
 import yaml
@@ -13,20 +13,68 @@ class GridMap(object):
 
         with open(config_file, 'r') as fp:
             cfg = yaml.load(fp)
-            self._map_name = cfp['name']
+            self._map_name = cfg['name']
             self._width = cfg['width']
             self._height = cfg['height']
             self._res = cfg['resolution']
 
+            self._size_x = int(np.ceil(self._width / self._res))
+            self._size_y = int(np.ceil(self._height / self._res))
+            self._mini_x = - int((self._width - self._res) / 2)
+            self._mini_y = - int((self._height - self._res) / 2)
+            self._maxi_x = int(self._width / 2)
+            self._maxi_y = int(self._height / 2)
+
+
         # Construct sdf map
-        self._sdf_map = np.array([self._height, self._width], np.float32)
+        self._sdf_map = np.array([self._size_y, self._size_x], np.float32)
         # Construct visit frequency map
-        self._freq_map = np.array([self._height, self._width], np.float32)
+        self._freq_map = np.array([self._size_y, self._size_x], np.float32)
+
+        # For test
+        self._occupancy_map = np.zeros([self._size_y, self._size_x], np.float32)
 
 
-    def MapOneScan(self, scan):
+    def MapOneScan(self, scan, pose):
+        x, y, yaw = pose
+
+        # Construct transform matrix
+        rot = np.identity(3, dtype=np.float32)
+        rot[0, 0] = np.cos(yaw)
+        rot[0, 1] = -np.sin(yaw)
+        rot[1, 0] = np.sin(yaw)
+        rot[1, 1] = np.cos(yaw)
+        # Translation vector
+        trans = np.array([[x, y, 0]])
+
+        # Transform points in robot frame to world frame
+        scan_w = np.dot(rot, scan) + np.tile(trans.transpose(),
+                                             (1, scan.shape[1]))
+        scan_w = scan[:2, :]
+
+        # Get the cell coordinates of scan hit points
+        scan_w_xs, scan_w_ys = self._FromMeterToCell(scan_w)
+
+        self._occupancy_map[scan_w_ys, scan_w_xs] = 1
+
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 1, 1)
+        plt.imshow(self._occupancy_map.T, origin='lower')
+        plt.show(block=True)
+
+
+    def VisualizeSdfMap(self):
         pass
 
 
-    def VisSDFMap(self):
-        pass
+    def _FromMeterToCell(self, scan):
+        """
+        Transform world scan in meter to world scan in cell coordinates.
+        """
+        xs = scan[0, :]
+        ys = scan[1, :]
+        # Convert from meters to cells
+        cell_xs = ((xs - self._mini_x) / self._res).astype(np.int16)
+        cell_ys = ((ys - self._mini_y) / self._res).astype(np.int16)
+        print cell_xs, cell_ys
+        return cell_xs, cell_ys
--- core/slam.py
+++ core/slam.py
@@ -26,22 +26,57 @@ class SLAM(object):
         if not os.path.exists(data_path):
             raise RuntimeError("File {} not found.".format(data_path))
 
+        # Construct 2D grid map
         self._grid_map = GridMap(FLAGS.map_config_path)
 
-        # Read data
+        # Read scan and pose data
         with open(FLAGS.data_path) as fp:
             data = pickle.load(fp)
-            self._scans, self._true_poses, self._times = data
+            self._scans, self._poses, self._times = data
 
+        # Initialization
+        self.Init()
+
+        # Scan angle range
+        self._scan_angles = np.arange(self._min_angle,
+                                      self._max_angle + self._res_angle,
+                                      self._res_angle)
+
+        # Estimated poses from SDF tracker
         self._est_poses = []
 
 
     def Init(self):
-        pass
+        self._GetScanSensorInfo()
+
+
+    def _GetScanSensorInfo(self):
+        if len(self._scans) == 0:
+            raise RuntimeError("No scan data is extracted.")
+        self._min_angle = self._scans[0][1]
+        self._res_angle = self._scans[0][2]
+        self._max_angle = self._scans[0][3]
+        self._min_range = self._scans[0][4]
+        self._max_range = self._scans[0][5]
+
+
+    def _ProcessScanToLocalCoords(self, scan):
+        print scan.shape
+        valid_idxs = np.logical_and((scan > self._min_range),
+                                    (scan < self._max_range))
+        data = scan[valid_idxs]
+        angles = self._scan_angles[valid_idxs]
+        x = data * np.cos(angles)
+        y = data * np.sin(angles)
+        z = np.ones(x.shape)
+        ret = np.stack((x, y, z))
+        return ret
 
 
     def Run(self):
-        pass
+        scan_data = np.array(self._scans[0][0])
+        scan_data = self._ProcessScanToLocalCoords(scan_data)
+        self._grid_map.MapOneScan(scan_data, self._poses[0])
 
 
 def main(argv):
